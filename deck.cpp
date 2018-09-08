f01671a3862e3d5fc6ac1f5fa8caec58d2689334
andrew.ross <amross.mail@gmail.com>
2018-09-08 13:23:03 +0100

    Added Deck

--- deck.cpp
+++ deck.cpp
@@ -1,6 +1,16 @@
 #include "deck.h"
 
+#include <Symbols/square.h>
+
 Deck::Deck()
+{
+    Square faceSymbol;
+    Modifier modifier;
+    Card newCard(faceSymbol, modifier);
+    mCards.append(newCard);
+}
+
+Card Deck::GetCard()
 {
 
 }
--- deck.h
+++ deck.h
@@ -1,7 +1,14 @@
+#include "card.h"
+#include <QVector>
+
 #pragma once
 
 class Deck
 {
 public:
     Deck();
+    Card GetCard();
+    void Shuffle();
+private:
+    QVector<Card> mCards;
 };
